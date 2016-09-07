02a4b42a8d3e7cc5897fe7d9015739cf8e5420e4
Jerome Sonnet (be-cloud.be) <jerome.sonnet@imply.lu>
2016-09-07 18:41:30 +0000

    crawler -> 1.0

--- server.js
+++ server.js
@@ -34,105 +34,121 @@ app.get('/scrape/:announcerId', function(req, res){
 
     announcersRef.child(req.params.announcerId).once("value", function(data) {
         var announcer = data.val();
-            var url = announcer.url;
+        var url = announcer.url;
         var crawlPattern = new RegExp(announcer.crawlPattern, "i");
         
         var properties = {};
         
-        var mainCrawler = Crawler(url);
+        winston.log('debug', 'Start crawling - ', {
+                announcerId : req.params.announcerId,
+                url : url,
+                crawlPattern : announcer.crawlPattern,
+        })
+        
+        var mainPages = [];
+        
+        var mainCrawler = new Crawler(url);
         
         mainCrawler.addFetchCondition(function(parsedURL) {
+            /*winston.log('debug', 'Query pattern for page - ', {
+                url : parsedURL,
+            })*/
             return parsedURL.path.match(crawlPattern);
         });
         
-        var mainPages = [];
-    
         mainCrawler.on("fetchcomplete", function(queueItem) {
-            mainPages.push(queueItem.url);
-        });
-    
-        mainCrawler.on("complete", function() {
-            winston.log('debug', 'All main pages crawled start crawling properties - ', {
-                pages : mainPages,
-                pageCount : mainPages.length,
-            })
-            
-            var promisses = [];
-            
-            for (i = 0; i < mainPages.length; i++) {
-                var page = mainPages[i];
-                winston.log('debug', 'Start crawling - ', {
-                    url : page,
+                winston.log('debug', 'Queue page - ', {
+                    url : queueItem.url,
                 })
-                var pageCrawler = Crawler(page);
-                pageCrawler.maxDepth = 2; // do not index properties recursively
-                pageCrawler.addFetchCondition(function(parsedURL) {
-                    return parsedURL.path.match(/[\/\w \.-]*[0-9]{7,7}$/i);
-                });
+                mainPages.push(queueItem.url);
+            });
+            
+        mainCrawler.on("complete", function() {
+                winston.log('debug', 'All main pages crawled start crawling properties - ', {
+                    pages : mainPages,
+                    pageCount : mainPages.length,
+                })
                 
-                pageCrawler.on("fetchcomplete", function(queueItem) {
-                    if(queueItem.url.match(/[\/\w \.-]*[0-9]{7,7}$/i)){
-                        winston.log('debug', 'Start crawling property - ', {
-                            url : queueItem.url,
-                        })
-                        request(queueItem.url,function(error, response, html) {
-                            if(!error){
-                                try {
-                                    var property_json = html.match(/initGoogleMap\((\[.*\])/i)[1];
-                                    var property = JSON.parse(property_json)[0];
-                                    //remove this for now as xml encoding fail
-                                    delete property.specifications;
-                                    
-                                    var $ = cheerio.load(html);
-                                    property.title = $("meta[name='og:title']").attr("content");
-                                    property.type = html.match(/googletag\.pubads\(\)\.setTargeting\("Type", \"(.*)\" \)/i)[1];
-                                    property.categories = queueItem.url.match(/http:\/\/www\.athome\.lu\/([\w \.-]*)\/([\w \.-]*)\/([\w \.-]*)\/([\w \.-]*)\/([\w \.-]*)/i);
-                                    winston.log('debug', 'Property crawled - ', {
-                                        title : property.title,
-                                    })
-                                    
-                                    property = _.pick(property, function(value, key) {return value != -1;})
-                                    
-                                    properties[property.id] = property;
-                                } catch(err) {
-                                    winston.log('debug', 'Error crawling property - ', {
-                                        error : err,
+                var promisses = [];
+                
+                for (i = 0; i < mainPages.length; i++) {
+                    var page = mainPages[i];
+                    winston.log('debug', 'Start crawling - ', {
+                        url : page,
+                    })
+                    var pageCrawler = new Crawler(page);
+                    pageCrawler.maxDepth = 2; // do not index properties recursively
+                    pageCrawler.addFetchCondition(function(parsedURL) {
+                        return parsedURL.path.match(/[\/\w \.-]*[0-9]{7,7}$/i);
+                    });
+                    
+                    pageCrawler.on("fetchcomplete", function(queueItem) {
+                        if(queueItem.url.match(/[\/\w \.-]*[0-9]{7,7}$/i)){
+                            winston.log('debug', 'Start crawling property - ', {
+                                url : queueItem.url,
+                            })
+                            request(queueItem.url,function(error, response, html) {
+                                if(!error){
+                                    try {
+                                        var property_json = html.match(/initGoogleMap\((\[.*\])/i)[1];
+                                        var property = JSON.parse(property_json)[0];
+                                        //remove this for now as xml encoding fail
+                                        delete property.specifications;
+                                        
+                                        var $ = cheerio.load(html);
+                                        property.atHomeUrl = queueItem.url;
+                                        property.title = $("meta[name='og:title']").attr("content");
+                                        property.type = html.match(/googletag\.pubads\(\)\.setTargeting\("Type", \"(.*)\" \)/i)[1];
+                                        property.categories = queueItem.url.match(/http:\/\/www\.athome\.lu\/([\w \.-]*)\/([\w \.-]*)\/([\w \.-]*)\/([\w \.-]*)\/([\w \.-]*)/i);
+                                        winston.log('debug', 'Property crawled - ', {
+                                            title : property.title,
+                                        })
+                                        
+                                        property = _.pick(property, function(value, key) {return value != -1;})
+                                        
+                                        properties[property.id] = property;
+                                    } catch(err) {
+                                        winston.log('debug', 'Error crawling property - ', {
+                                            error : err,
+                                        })
+                                    }
+                                } else {
+                                    winston.log('debug', 'Error getting the property - ', {
+                                        error : error,
                                     })
                                 }
-                            } else {
-                                winston.log('debug', 'Error getting the property - ', {
-                                    error : error,
-                                })
-                            }
-                        });
-                    }
-                });
+                            });
+                        }
+                    });
+                    
+                    var deferred = Q.defer();
+                    promisses.push(deferred.promise);
+                    pageCrawler.deferred = deferred;
+                    pageCrawler.on("complete", function() {
+                        winston.log('debug', 'End page crawling - ', {
+                            url : this.initialPath,
+                        })
+                        this.deferred.resolve();
+                    });
+                    
+                    pageCrawler.start();
+                }
                 
-                var deferred = Q.defer();
-                promisses.push(deferred.promise);
-                pageCrawler.deferred = deferred;
-                pageCrawler.on("complete", function() {
-                    winston.log('debug', 'End page crawling - ', {
-                        url : this.initialPath,
+                Q.all(promisses).then(function() {
+                    winston.log('debug', 'Crawling properties is finished - ', {
+                        propertyCount : properties.length,
                     })
-                    this.deferred.resolve();
-                });
-                
-            }
-            
-            Q.all(promisses).then(function() {
-                winston.log('debug', 'Crawling properties is finished - ', {
-                    propertyCount : properties.length,
+                    propertiesRef.child(req.params.announcerId).set(properties).then(function() {
+                        res.json(properties);
+                    });
                 })
-                propertiesRef.child(req.params.announcerId).set(properties).then(function() {
-                    res.json(properties);
-                });
             });
-        });
+            
+            mainCrawler.start();
     });
 });
 
-server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
+server.listen(8081  || 3000, process.env.IP || "0.0.0.0", function(){
     var addr = server.address();
     console.log("Connect to ", addr.address + ":" + addr.port);
 });
